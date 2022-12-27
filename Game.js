3e5477787fb6ab28671e2a2e4be57fb10bcefc78
judeallanhill <judeallanhill@icloud.com>
2022-12-27 08:44:23 -0800

    Adding Fog And Glitchy Points

--- Game.js
+++ Game.js
@@ -8,11 +8,14 @@ let Player = {
     gravity: 5,
     lives: 1,
     posx: 25,
-    posy: 375,
+    posy: 400,
     grounded: true,
     velocity: -0.1,
     speed:0.01,
 }
+let setHeight = null;
+let setWidth = null;
+
 function GravityFalling() {
     if (Player.grounded === false){
         Player.speed = Player.velocity + Player.speed
@@ -21,18 +24,19 @@ function GravityFalling() {
         Player.posy -= 100;
         Player.grounded = false;
     }
-    if (Player.posy >= 375) {
+    if (Player.posy >= 400) {
         // console.log("GROUND")
         Player.grounded = true
-        Player.posy = 375
+        Player.posy = 400
         Player.speed = 0.01
     }
     if (Player.grounded === false) {
         Player.posy -= Player.speed
     }
-    if (Player.posy >= Platform.posy && Player.posx >= Platform.posx) {
+    if (Player.posy >= Platform.posy && Player.posx <= Platform.posx) {
         Player.grounded = true
-        console.log("FLOOR")
+        Player.speed = 0.01
+        // console.log("FLOOR")
     }
     // console.log("Vel" + Player.velocity)
     // console.log("Speed" + Player.speed)
@@ -42,18 +46,58 @@ let Platform = {
     posx: 40,
     posy: 340,
 }
-function PLATFORM() {
+let FogStats = {
+    fogH: 10,
+    fogW: 10000,
+    Transparence: 0.5,
+    Speed:0.001,
+    point:0,
+}
+function FogIncrease() {
+    for (let i = 0; i < 100; i++) {
+        FogStats.fogH+=FogStats.Speed;
+
+    }
+}
+function Points() {
+    
+        for (let i = 0; i < 50; i++) {
+            FogStats.point += 0.001;
+            console.log(Math.round(FogStats.point))
+            ctx.textAlign = "center";
+            ctx.font="30px Comic Sans MS";
+            console.log(Player.alive)
+            ctx.fillText("Points: " + Math.round(FogStats.point), canvas.width/2, 100);     
+        }
+}
+function CheckFogHeight(){ 
+    console.log(400 - Player.posy)
+    if (FogStats.fogH >= 50 + 400 - Player.posy) {
+        Platform.alive = false;
+        console.log("DEAD")
+        ctx.textAlign = "center";
+        ctx.font="30px Comic Sans MS";
+        ctx.fillText("You Died", canvas.width/2, canvas.height/2);     
+    }
+}
+function FOG(){
+    ctx.fillStyle = "gray"
+    ctx.globalAlpha = FogStats.Transparence;
+    ctx.fillRect(0,400 - FogStats.fogH,FogStats.fogW,FogStats.fogH)
+    ctx.globalAlpha = 1.0;
+    ctx.fillStyle = "black"
+}function PLATFORM() {
     ctx.fillStyle = "red";
-    ctx.fillRect(Platform.posx - 25,Platform.posy - 5,50,10)
+    ctx.fillRect(Platform.posx - 40,Platform.posy - 5,80,10)
     ctx.fillStyle = "black"
 }
 function movePlayer() {
     if (cureentKeys.get("d") === true) {
-        console.log("D")
+        // console.log("D")
         Player.posx+=5;
     }
     if (cureentKeys.get("a") === true) {
-        console.log("A")
+        // console.log("A")
         Player.posx-=5;
     }
 }
@@ -69,13 +113,17 @@ function setupKeyboard() {
 }
 function DrawPlayer() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);    
-    ctx.fillRect(Player.posx - 25,Player.posy - 25,50,50);
+    ctx.fillRect(Player.posx - 25,Player.posy - 50,50,50);
 }
 function LOOP() {
     GravityFalling();
     DrawPlayer();
     movePlayer();
     PLATFORM();
+    FOG();
+    FogIncrease();
+    CheckFogHeight();
+    Points();
     window.requestAnimationFrame(LOOP);
 }
 setupKeyboard();
